242de5d52677c507d0c8333b7103d9b6a3c329e1
ASUS <dheepdev01@gmail.com>
2020-05-17 19:04:10 +0530

    Final Commit with given tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Ball(),
       ),
     );
+class Ball extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green.shade300,
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade900,
+        title: Text('Ask Me Anything'),
+        centerTitle: true,
+      ),
+      body: BallBody(),
+    );
+  }
+}
+
+class BallBody extends StatefulWidget {
+  @override
+  _BallBodyState createState() => _BallBodyState();
+}
+
+class _BallBodyState extends State<BallBody> {
+  int ballNumber=1;
+  @override
+  Widget build(BuildContext context) {
+    return Center (
+    child: FlatButton(
+      onPressed: () {
+        setState(() {
+          ballNumber = Random().nextInt(5) +1 ;
+        });
+      },
+      child: Image.asset('images/ball$ballNumber.png'),
+    ),
+    );
+  }
+}
+
